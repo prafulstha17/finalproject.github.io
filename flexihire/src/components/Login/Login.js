5a4098c4a5b1e00c1d661b019023687f4faf72a8
prafulstha17 <128771633+prafulstha17@users.noreply.github.com>
2023-04-15 16:51:08 +0545

    add bootstrap
    add login link

--- flexihire/src/components/Login/Login.js
+++ flexihire/src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import { auth } from "../../confg/firebase";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
 
 
 function Login() {
@@ -9,6 +10,7 @@ function Login() {
 
   const [email,setEmail]=useState("")
   const[password,setPassword]= useState("")
+  const navitage = useNavigate();
 
   // handleLogin
   const handleLogin =(e)=>{
@@ -19,7 +21,9 @@ signInWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
     // Signed in 
     const user = userCredential.user;
-    console.log(user);
+
+	// navitageAfterSuccesfullyLoginToNextPage
+    navitage("/"); //homepage 
     // ...
   })
   .catch((error) => {
@@ -29,67 +33,59 @@ signInWithEmailAndPassword(auth, email, password)
 
 
   return (
-    <div class="center-wrap">
-											<div class="section text-center">
-												<h4 class="mb-4 pb-3" id="color-gradient">Log In</h4>
-												<div class="form-group">
-													<input type="email" name="logemail" class="form-style"
+    <div className="center-wrap">
+											<div className="section text-center">
+												<h4 className="mb-4 pb-3" id="color-gradient">Log In</h4>
+												<div className="form-group">
+													<input type="email" name="logemail" className="form-style"
 														placeholder="Your Email" id="logemail" autocomplete="off"
 														required
                             onChange={e=>setEmail(e.target.value)}
                             />
-													<i class="input-icon uil uil-at"></i>
+													<i className="input-icon uil uil-at"></i>
 												</div>
-												<div class="form-group mt-2">
-													<input type="password" name="logpass" class="form-style"
+												<div className="form-group mt-2">
+													<input type="password" name="logpass" className="form-style"
 														placeholder="Your Password" id="logpass" autocomplete="off"
 														required 
                             onChange={e=>setPassword(e.target.value)}
                             />
-													<i class="input-icon uil uil-lock-alt"></i>
+													<i className="input-icon uil uil-lock-alt"></i>
 												</div>
-												<div class="side mb-0 pb-3">
+												<div className="side mb-0 pb-3">
 													<span>
-														<div class="form-check">
+														<div className="form-check">
 															<input type="checkbox" name="rememberme"
-																class="form-checkbox" id="rememberme"/>
+																className="form-checkbox" id="rememberme"/>
 															<p for="rememberme" id="color-gradient">Remember me</p>
 														</div>
 													</span>
 													<span>
-														<p><a href="#0" class="link" id="color-gradient">Forgot your
+														<p><a href="#0" className="link" id="color-gradient">Forgot your
 																password?</a></p>
 													</span>
 												</div>
-												<button class="btn btn-info btn-lg btn-block btn-outline-danger "
+												<button className="btn btn-info btn-lg btn-block btn-outline-danger "
 													type="submit" onClick={handleLogin}>Login</button>
                                                       {error && <span>Wrong email or password</span>}
 
-												<p class="or mt-4 text-center" id="color-gradient">Or login with</p>
-												<ul class="social-links">
+												<p className="or mt-4 text-center" id="color-gradient">Or login with</p>
+												<ul className="social-links">
 													<li><a href="#" id="color-gradient"><i
-																class="fab fa-google"></i></a>
+																className="fab fa-google"></i></a>
 													</li>
 													<li><a href="#" id="color-gradient"><i
-																class="fab fa-facebook-f"></i></a></li>
+																className="fab fa-facebook-f"></i></a></li>
 													<li><a href="#" id="color-gradient"><i
-																class="fab fa-twitter"></i></a>
+																className="fab fa-twitter"></i></a>
 													</li>
 												</ul>
-												<p class="mb-0 mt-4 text-center"><a href="#" class="join"
+												<p className="mb-0 mt-4 text-center"><a href="#" className="join"
 														onclick="create()" id="color-gradient">Not a member?
 														Sign up</a></p>
 											</div>
 										</div>
-    // <div className='login'>
-      
-    //   <form onSubmit={handleLogin}>
-    //     <input type='email' placeholder='email' onChange={e=>setEmail(e.target.value)}/>
-    //     <input type='password' placeholder='password' onChange={e=>setPassword(e.target.value)} />
-    //     <button type='submit' >Login</button>
-    //     {error && <span>Wrong email or password</span>}
-    //   </form>
-    // </div>
+
   )
 }
 
--- flexihire/src/components/Signup/SignUp.js
+++ flexihire/src/components/Signup/SignUp.js
@@ -33,49 +33,46 @@ function SignUp() {
 
 
   return (
-    <div class="card-back" id="card">
-                <div class="center-wrap">
-                  <div class="section text-center">
-                    <h4 class="mb-4 pb-3" id="color-gradient">Sign Up</h4>
-                    <div class="form-group">
-                      <input type="text" name="logname" class="form-style"
+    <div className="card-back" id="card">
+                <div className="center-wrap">
+                  <div className="section text-center">
+                    <h4 className="mb-4 pb-3" id="color-gradient">Sign Up</h4>
+                    <div className="form-group">
+                      <input type="text" name="logname" className="form-style"
                         placeholder="Your Full Name" id="logname" autocomplete="off"/>
-                      <i class="input-icon uil uil-user"></i>
+                      <i className="input-icon uil uil-user"></i>
                     </div>
-                    <div class="form-group mt-2">
-                      <input type="email" name="logemail" class="form-style"
+                    <div className="form-group mt-2">
+                      <input type="email" name="logemail" className="form-style"
                         placeholder="Your Email" id="logemail" onChange={(e) => setEmail(e.target.value)}
                         autocomplete="off" required/>
-                      <i class="input-icon uil uil-at"></i>
+                      <i className="input-icon uil uil-at"></i>
                     </div>
-                    <div class="form-group mt-2">
-                      <input type="password" name="logpass" class="form-style"
+                    <div className="form-group mt-2">
+                      <input type="password" name="logpass" className="form-style"
                         placeholder="Your Password" id="logpass" onChange={(e) => setPassword(e.target.value)}
                         autocomplete="off" required/>
-                      <i class="input-icon uil uil-lock-alt"></i>
+                      <i className="input-icon uil uil-lock-alt"></i>
                     </div>
                     <button href="#"
-                      class="btn btn-info btn-lg btn-block btn-outline-danger mt-4" onClick={signIn}>sign
+                      className="btn btn-info btn-lg btn-block btn-outline-danger mt-4" onClick={signIn}>sign
                       up</button>
-                    <p class="or mt-4 text-center" id="color-gradient">Or signup with</p>
-                    <ul class="social-links">
+                    <p className="or mt-4 text-center" id="color-gradient">Or signup with</p>
+                    <ul className="social-links">
                       <li><a href="#" id="color-gradient"><i
-                            class="fab fa-google"></i></a>
+                            className="fab fa-google" onClick={signInWithGoogle}></i></a>
                       </li>
                       <li><a href="#" id="color-gradient"><i
-                            class="fab fa-facebook-f"></i></a></li>
+                            className="fab fa-facebook-f"></i></a></li>
                       <li><a href="#" id="color-gradient"><i
-                            class="fab fa-twitter"></i></a>
+                            className="fab fa-twitter"></i></a>
                       </li>
                     </ul>
-                    <p class="mb-0 mt-4 text-center"><a href="#" class="login"
+                    <p className="mb-0 mt-4 text-center"><a href="#" className="login"
                         onclick="enter()" id="color-gradient">Have an account?
                         Log in</a></p>
                   </div>
-                  <div>
                   
-                  <button type='submit' onClick={signInWithGoogle}> Sign In With Google</button>
-                  </div>
                 </div>
               </div>
   )
