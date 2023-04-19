0c43051b330d7a5161afefdf7dd7d0f0254cf89a
AxelXrest <52139213+AxelXrest@users.noreply.github.com>
2023-04-19 09:53:40 +0545

    updated App.js
    added navbars and contents

--- flexihire/src/App.js
+++ flexihire/src/App.js
@@ -1,15 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import Home from './components/Home/Home'
-import SignUp from './components/Signup/SignUp'
-import Login from './components/Login/Login'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import { auth } from './confg/firebase'
-import SignOut from './components/Signup/SignOut'
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { auth } from './confg/firebase';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Profile from './components/Profile/Profile';
+import SignUp from './components/Signup/SignUp';
+import './login_signup.css';
+import './responsive.css';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+
+    // unsubscribe from the listener when the component unmounts
+    return unsubscribe;
+  }, []);
 
   const [userName, setUserName] = useState("");
-  
+
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       if (user) {
@@ -17,20 +29,70 @@ function App() {
       } else setUserName("");
     });
   }, []);
+
   return (
-    <>
-    <BrowserRouter>
-      <Routes>
-       <Route path='/signup' element={<SignUp/>}/>
-       <Route path='/login' element={<Login/>}/>
-       <Route path="/" element={<Home name={userName} />} />
-      </Routes>
-      
-    </BrowserRouter>
-    
-    </>
-    
-  )
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container">
+            <Link className="navbar-brand" to="/">
+              FlexiHire
+            </Link>
+            <button
+              className="navbar-toggler"
+              data-toggle="collapse"
+              data-target="#navbarToggler"
+              aria-controls="navbarToggler"
+              aria-label="Toggle navigation" 
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarToggler">
+              <ul className="navbar-nav ml-auto">
+                {user ? (
+                  <>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/profile">
+                        Profile
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <button onClick={() => auth.signOut()} className="nav-link btn btn-link">
+                        Logout
+                      </button>
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/sign-in">
+                        Login
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/sign-up">
+                        Sign up
+                      </Link>
+                    </li>
+                  </>
+                )}
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="auth-wrapper">
+          <div className="auth-inner container mt-5 mb-5">
+            <Routes>
+              <Route path="/" element={<Home name={userName} />} />
+              <Route path="/sign-in" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />
+              <Route path="/profile" element={<Profile user={user} />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
