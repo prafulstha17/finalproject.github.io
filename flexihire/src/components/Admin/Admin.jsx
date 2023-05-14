519a658cacc77bcf2e4dc1f2757cbcd0f3a57bc6
Axel Xrest <52139213+AxelXrest@users.noreply.github.com>
2023-05-14 19:24:14 +0545

    admin and silhouette

--- flexihire/src/components/Admin/Admin.jsx
+++ flexihire/src/components/Admin/Admin.jsx
@@ -1,9 +1,121 @@
-function Admin(){
-    return(
-        <div className="admin-panel">
-            0
-        </div>
-    );
+import "./Admin.css";
+
+function Admin() {
+  return (
+    <div className="admin-panel">
+      <header class="page-header">
+        <nav>
+          <ul class="admin-menu">
+            <li class="menu-heading">
+              <h3>Admin</h3>
+            </li>
+            <li>
+              <a href="#0">
+                <i class="fa-regular fa-browsers"></i>
+                <span>Pages</span>
+              </a>
+            </li>
+            <li>
+              <a href="#0">
+                <i class="fa-solid fa-user"></i>
+                <span>Users</span>
+              </a>
+            </li>
+            <li>
+              <a href="#0">
+                <i class="fa-regular fa-chart-mixed"></i>
+                <span>Trends</span>
+              </a>
+            </li>
+            <li>
+              <a href="#0">
+                <i class="fa-solid fa-option"></i>
+                <span>Collection</span>
+              </a>
+            </li>
+            <li>
+              <a href="#0">
+                <i class="fa-regular fa-message-captions"></i>
+                <span>Comments</span>
+              </a>
+            </li>
+            <li>
+              <a href="#0">
+                <i class="fa-solid fa-brush"></i>
+                <span>Appearance</span>
+              </a>
+            </li>
+            <li class="menu-heading">
+              <h3>Settings</h3>
+            </li>
+            <li>
+              <a href="#0">
+                <i class="fa-solid fa-gear"></i>
+                <span>Settings</span>
+              </a>
+            </li>
+            <li>
+              <a href="#0">
+                <i class="fa-solid fa-bars"></i>
+                <span>Options</span>
+              </a>
+            </li>
+            <li>
+              <a href="#0">
+                <i class="fa-solid fa-bars"></i>
+                <span>Charts</span>
+              </a>
+            </li>
+            <li>
+              <div class="switch">
+                <input type="checkbox" id="mode" checked />
+                <label for="mode">
+                  <span></span>
+                  <span>Dark</span>
+                </label>
+              </div>
+              <button
+                class="collapse-btn"
+                aria-expanded="true"
+                aria-label="collapse menu"
+              >
+                <i class="fa-solid fa-arrow-left"></i>
+                <span>Collapse</span>
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <section class="page-content">
+        <section class="search-and-user">
+          <form>
+            <input type="search" placeholder="Search Pages..." />
+            <button type="submit" aria-label="submit form">
+              <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
+          </form>
+          <div class="admin-profile">
+            <span class="greeting">Hello admin</span>
+            <div class="notifications">
+              <span class="badge">1</span>
+
+              <i class="fa-solid fa-user"></i>
+            </div>
+          </div>
+        </section>
+        <section class="grid">
+          <article></article>
+          <article></article>
+          <article></article>
+          <article></article>
+          <article></article>
+          <article></article>
+          <article></article>
+          <article></article>
+        </section>
+      </section>
+    </div>
+  );
 }
 
 export default Admin;
--- flexihire/src/components/PageFooter/PageFooter.jsx
+++ flexihire/src/components/PageFooter/PageFooter.jsx
@@ -1,95 +1,125 @@
-
-import "./PageFooter.css"
+import "./PageFooter.css";
+import silhouette from "../Icon/silhouette/silhouette.png";
 
 function PageFooter() {
-
-   return (
-      <div className="footerBody">
-         <footer >
-            <div class="footercontainer">
-               <div class="row">
-
-                  <div className="col">
-                     <h4>For Client</h4>
-                     <div class="links">
-                        <a href="#">How to Hire</a>
-                        <a href="#">Talent Marketplace</a>
-                        <a href="#">Project Catalog</a>
-                        <a href="#">Hire an Agency</a>
-                        <a href="#">Any Hire</a>
-                        <a href="#">Direct Contracts</a>
-                     </div>
-
-                  </div>
-                  <div className="col">
-                     <h4>For Talent</h4>
-                     <div className="links">
-                        <a href="#">How to Find Work</a>
-                        <a href="#">Direct Contracts</a>
-                        <a href="#">Find Freelance Jobs Worldwide</a>
-                        <a href="#">Find Freelance Jobs in the Nepal</a>
-                     </div>
-
-                  </div>
-
-                  <div class="col" id="useful-links">
-                     <h4>About</h4>
-                     <div class="links">
-                        <a href="#">How it Work</a>
-                        <a href="/services">Services</a>
-                        <a href="#">Privacy Policy</a>
-                        <a href="/termsCondition">Terms & Condition </a>
-                        <a href="/whyUs">Why Us?</a>
-                     </div>
-                  </div>
-
-                  <div class="col" id="support">
-                     <h4>Support</h4>
-                     <div class="support-details">
-                        <i class="fa-solid fa-envelope"></i>
-                        <p>flexhirenepal@gmail.com</p>
-                     </div><br />
-                     <div class="support-details">
-                        <i class="fa fa-phone"></i>
-                        <p>+977 98012345678</p>
-                     </div>
-                  </div>
-               </div>
-
-               <div className="col" id='followUs'>
-                  <h4>Follow Us</h4>
-                  <div class="social">
-                     <a href="https://www.facebook.com/profile.php?id=100092199707911" target='_blank'><i class="fab fa-facebook fa-2xl"></i></a>
-                     <a href="https://www.instagram.com/hireflexi/" target='_blank'><i class="fab fa-instagram fa-2xl"></i></a>
-                     <a href="https://www.youtube.com/channel/UC0ntZYXMAsvcgrKLWyHMe5Q" target='_blank'><i class="fab fa-youtube fa-2xl"></i></a>
-                     <a href="https://twitter.com/HireFlexi" target='_blank'><i class="fab fa-twitter fa-2xl"></i></a>
-                     <a href="https://www.linkedin.com/in/flexi-hire-8437a0274" target='_blank'><i class="fab fa-linkedin fa-2xl"></i></a>
-                  </div>
-               </div>
-
-               <div class="container">
-                  <div class="copyright">
-                     <div class="row">
-                        <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                           &copy; <a class="border-bottom" href="#">FlexiHire</a>, All Right Reserved.
-                           <br />Designed By <a class="border-bottom" href="">Ajay Nemkul Shrestha</a> & <a class="border-bottom" href=""> Praful Shrestha</a>
-                        </div>
-                        <div class="col-md-6 text-center text-md-end">
-                           <div class="footer-menu">
-                              <a href="">Home</a>
-                              <a href="">Cookies</a>
-                              <a href="">Help</a>
-                              <a href="">FAQs</a>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
+  return (
+    <div className="footerBody">
+      <div className="silhouette">
+        <img className="silhouette" src={silhouette}/>
+      </div>
+      <footer>
+        <div class="footercontainer">
+          <div class="row">
+            <div className="col">
+              <h4>For Client</h4>
+              <div class="links">
+                <a href="#">How to Hire</a>
+                <a href="#">Talent Marketplace</a>
+                <a href="#">Project Catalog</a>
+                <a href="#">Hire an Agency</a>
+                <a href="#">Any Hire</a>
+                <a href="#">Direct Contracts</a>
+              </div>
+            </div>
+            <div className="col">
+              <h4>For Talent</h4>
+              <div className="links">
+                <a href="#">How to Find Work</a>
+                <a href="#">Direct Contracts</a>
+                <a href="#">Find Freelance Jobs in the Nepal</a>
+              </div>
             </div>
 
-         </footer >
-      </div >
-   )
+            <div class="col" id="useful-links">
+              <h4>About</h4>
+              <div class="links">
+                <a href="/admin">How it Work</a>
+                <a href="/services">Services</a>
+                <a href="#">Privacy Policy</a>
+                <a href="/termsCondition">Terms & Condition </a>
+                <a href="/whyUs">Why Us?</a>
+              </div>
+            </div>
+
+            <div class="col" id="support">
+              <h4>Support</h4>
+              <div class="support-details">
+                <i class="fa-solid fa-envelope"></i>
+                <p>flexhirenepal@gmail.com</p>
+              </div>
+              <br />
+              <div class="support-details">
+                <i class="fa fa-phone"></i>
+                <p>+977 98012345678</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col" id="followUs">
+            <h4>Follow Us</h4>
+            <div class="social">
+              <a
+                href="https://www.facebook.com/profile.php?id=100092199707911"
+                target="_blank"
+              >
+                <i class="fab fa-facebook fa-2xl"></i>
+              </a>
+              <a href="https://www.instagram.com/hireflexi/" target="_blank">
+                <i class="fab fa-instagram fa-2xl"></i>
+              </a>
+              <a
+                href="https://www.youtube.com/channel/UC0ntZYXMAsvcgrKLWyHMe5Q"
+                target="_blank"
+              >
+                <i class="fab fa-youtube fa-2xl"></i>
+              </a>
+              <a href="https://twitter.com/HireFlexi" target="_blank">
+                <i class="fab fa-twitter fa-2xl"></i>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/flexi-hire-8437a0274"
+                target="_blank"
+              >
+                <i class="fab fa-linkedin fa-2xl"></i>
+              </a>
+            </div>
+          </div>
+
+          <div class="container">
+            <div class="copyright">
+              <div class="row">
+                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                  &copy;{" "}
+                  <a class="border-bottom" href="#">
+                    FlexiHire
+                  </a>
+                  , All Right Reserved.
+                  <br />
+                  Designed By{" "}
+                  <a class="border-bottom" href="">
+                    Ajay Nemkul Shrestha
+                  </a>{" "}
+                  &{" "}
+                  <a class="border-bottom" href="">
+                    {" "}
+                    Praful Shrestha
+                  </a>
+                </div>
+                <div class="col-md-6 text-center text-md-end">
+                  <div class="footer-menu">
+                    <a href="">Home</a>
+                    <a href="">Cookies</a>
+                    <a href="">Help</a>
+                    <a href="">FAQs</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
 export default PageFooter;
