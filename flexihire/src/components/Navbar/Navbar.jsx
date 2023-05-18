049bca54073edaf497ecafec8aa5ae67060f7ffb
prafulstha17 <128771633+prafulstha17@users.noreply.github.com>
2023-05-18 18:00:29 +0545

    add nav bar css
    add nav bar css

--- flexihire/src/components/Navbar/Navbar.jsx
+++ flexihire/src/components/Navbar/Navbar.jsx
@@ -56,98 +56,108 @@ function Navbar() {
   return (
     <>
       <nav className="navbar sticky-top">
-        <div class="menu-icon">
-          <span class="fas fa-bars"></span>
-        </div>
-        <div class="logo">
-          <a className="navbar-brand" href="/">
-            <div className="brand-logo">
-              <img src={transparent_bg} alt="brand-logo" />
-            </div>
-            <div className="brand-name">FlexiHire</div>
-          </a>
-        </div>
-        <div class="nav-items">
-          <li className="nav-item">
-            <a className="nav-link" href="/jobs" onClick={handleLinkClick}>
-              Jobs
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/services" onClick={handleLinkClick}>
-              Services
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/contactUs" onClick={handleLinkClick}>
-              Contact Us
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/aboutUs" onClick={handleLinkClick}>
-              About Us
-            </a>
-          </li>
-        </div>
-        <div className="login-search">
-          <div className="search-form">
-            <div class="search-icon">
-              <i class="fa-solid fa-magnifying-glass"></i>
-            </div>
-            <div class="cancel-icon">
-              <i class="fa-solid fa-xmark" style={{ color: "#ffffff" }} />
-            </div>
-            <form action="#">
-              <button type="submit" class="fas fa-search"></button>
-              <input
-                type="search"
-                class="search-data"
-                placeholder="Search"
-                required
-              />
-              <div class="input-select">
-                <select data-trigger="" name="choices-single-default">
-                  <option placeholder="">Flexer</option>
-                  <option>FlexiHirer</option>
-                </select>
+          <div class="menu-icon">
+            <span class="fas fa-bars"></span>
+          </div>
+          <div class="logo">
+            <a className="navbar-brand" href="/">
+              <div className="brand-logo">
+                <img src={transparent_bg} alt="brand-logo" />
               </div>
-            </form>
+              <div className="brand-name">FlexiHire</div>
+            </a>
           </div>
-          <div class="nav-items-case">
-            {user && (
-              <Dropdown>
-                <div className="loggedIn">
-                  <Dropdown.Toggle variant="link" id="dropdown-basic">
-                    <p>User</p>
-                    <i class="fa-solid fa-user fa-l"></i>
-                  </Dropdown.Toggle>
-
-                  <Dropdown.Menu className="dropdown-menu">
-                    <Dropdown.Item href="/profile">Profile</Dropdown.Item>
-                    <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
-                  </Dropdown.Menu>
-                </div>
-              </Dropdown>
-            )}
-            {!user && (
-              <Dropdown>
-                <div className="loggedOut">
-                  <Dropdown.Toggle variant="link" id="dropdown-basic">
-                    <p>Members Area</p>
-                    <i class="fa-solid fa-user fa-l"></i>
-                  </Dropdown.Toggle>
-
-                  <Dropdown.Menu className="dropdown-menu">
-                    <Dropdown.Item href="/sign-in">Login</Dropdown.Item>
-                    <Dropdown.Item href="/sign-up">
-                      Create Account
-                    </Dropdown.Item>
-                  </Dropdown.Menu>
-                </div>
-              </Dropdown>
-            )}
+          <div class="nav-items">
+            <li className="nav-item">
+              <a className="nav-link" href="/jobs" onClick={handleLinkClick}>
+                Jobs
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="/services"
+                onClick={handleLinkClick}
+              >
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="/contactUs"
+                onClick={handleLinkClick}
+              >
+                Contact Us
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/aboutUs" onClick={handleLinkClick}>
+                About Us
+              </a>
+            </li>
           </div>
-        </div>
+          <div className="login-search">
+            <div className="search-form">
+              <div class="search-icon">
+                <i class="fa-solid fa-magnifying-glass"></i>
+              </div>
+              <div class="cancel-icon">
+                <i class="fa-solid fa-xmark" style={{ color: "#ffffff" }} />
+              </div>
+              <form action="#">
+                <button type="submit" class="fas fa-search"></button>
+                <input
+                  type="search"
+                  class="search-data"
+                  placeholder="Search"
+                  required
+                />
+                <div class="input-select">
+                  <select data-trigger="" name="choices-single-default">
+                    <option placeholder="">Flexer</option>
+                    <option>FlexiHirer</option>
+                  </select>
+                </div>
+              </form>
+            </div>
+            <div class="nav-items-case">
+              {user && (
+                <Dropdown>
+                  <div className="loggedIn">
+                    <Dropdown.Toggle variant="link" id="dropdown-basic">
+                      <p>User</p>
+                      <i class="fa-solid fa-user fa-l"></i>
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu className="dropdown-menu">
+                      <Dropdown.Item href="/profile">Profile</Dropdown.Item>
+                      <Dropdown.Item onClick={handleLogout}>
+                        Logout
+                      </Dropdown.Item>
+                    </Dropdown.Menu>
+                  </div>
+                </Dropdown>
+              )}
+              {!user && (
+                <Dropdown>
+                  <div className="loggedOut">
+                    <Dropdown.Toggle variant="link" id="dropdown-basic">
+                      <p>Members Area</p>
+                      <i class="fa-solid fa-user fa-l"></i>
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu className="dropdown-menu">
+                      <Dropdown.Item href="/sign-in">Login</Dropdown.Item>
+                      <Dropdown.Item href="/sign-up">
+                        Create Account
+                      </Dropdown.Item>
+                    </Dropdown.Menu>
+                  </div>
+                </Dropdown>
+              )}
+            </div>
+          </div>
       </nav>
     </>
   );
