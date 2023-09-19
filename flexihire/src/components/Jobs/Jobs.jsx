861b41c566c08d4d9c10d5e143d114c5cf3650a3
prafulstha17 <128771633+prafulstha17@users.noreply.github.com>
2023-09-19 17:24:38 +0545

    datadisplay
    data display

--- flexihire/src/components/Jobs/Jobs.jsx
+++ flexihire/src/components/Jobs/Jobs.jsx
@@ -1,9 +1,14 @@
+
+import DisplayData from '../ContactForm/DisplayData'
 import './Jobs.css'
 
 function Jobs() {
 
     return (
+       
         <div class="container-xxl py-5">
+             <DisplayData/>
+
             <div class="container">
                 <h1 class="text-center mb-5 wow fadeInUp" data-wow-delay="0.1s">Job Listing</h1>
                 <div class="tab-class text-center wow fadeInUp" data-wow-delay="0.3s">
