ede04916265c094c596314645ec018ab48b6cede
Axel Xrest <52139213+AxelXrest@users.noreply.github.com>
2023-12-06 23:23:44 +0545

    chat-UI
    not completely functional but interface design

--- flexihire/src/components/Message/Message.jsx
+++ flexihire/src/components/Message/Message.jsx
@@ -1,24 +1,28 @@
-// Import necessary modules and components
 import React, { useState, useEffect } from "react";
-import { auth, db } from "../../config/firebase";
+import { auth, db, storage } from "../../config/firebase";
 import {
   collection,
   addDoc,
-  serverTimestamp,
   getDocs,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  orderBy,
+  where,
 } from "firebase/firestore";
+import { ref, getDownloadURL } from 'firebase/storage';
 import "./Message.css";
 
-// Message component definition
 function Message() {
-  // State for user authentication and chat box
   const [user, setUser] = useState(null);
   const [isExpanded, setIsExpanded] = useState(false);
   const [messageText, setMessageText] = useState("");
   const [users, setUsers] = useState([]);
   const [selectedUser, setSelectedUser] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [profileImages, setProfileImages] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  // Effect to set user state based on authentication
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setUser(user);
@@ -26,7 +30,6 @@ function Message() {
     return unsubscribe;
   }, []);
 
-  // Effect to fetch users from the database
   useEffect(() => {
     async function fetchUsers() {
       try {
@@ -34,6 +37,7 @@ function Message() {
         const usersSnapshot = await getDocs(usersCollection);
         const usersData = usersSnapshot.docs.map((doc) => ({
           id: doc.id,
+          userId: doc.data().userId,
           ...doc.data(),
         }));
         setUsers(usersData);
@@ -45,44 +49,101 @@ function Message() {
     fetchUsers();
   }, []);
 
-  // Function to handle chat box expansion
+  useEffect(() => {
+    async function fetchProfileImages() {
+      const imagePromises = users.map(async (user) => {
+        const profileImageRef = ref(storage, `users/${user.userId}/profile.jpg`);
+        try {
+          const profileImageUrl = await getDownloadURL(profileImageRef);
+          return { userId: user.userId, imageUrl: profileImageUrl };
+        } catch (error) {
+          console.log(`Profile image not found for user ${user.userId}.`);
+          return { userId: user.userId, imageUrl: null };
+        }
+      });
+  
+      // Wait for all promises to resolve
+      const images = await Promise.all(imagePromises);
+  
+      // Set profile images for all users
+      const profileImagesMap = images.reduce((acc, { userId, imageUrl }) => {
+        acc[userId] = imageUrl;
+        return acc;
+      }, {});
+  
+      setProfileImages(profileImagesMap);
+    }
+  
+    // Introduce a 3-second delay before setting loading to false
+    const delayTimeout = setTimeout(() => {
+      setLoading(false); // Set loading to false once images are loaded
+    }, 3000);
+  
+    // Fetch profile images
+    fetchProfileImages();
+  
+    // Clear the delay timeout on component unmount
+    return () => clearTimeout(delayTimeout);
+  }, [users]);
+  
+
+
+  useEffect(() => {
+    if (selectedUser) {
+      const q = query(
+        collection(db, "messages"),
+        orderBy("timestamp"),
+        where("users", "array-contains", user.uid)
+      );
+
+      const unsubscribe = onSnapshot(q, (snapshot) => {
+        const messagesData = snapshot.docs
+          .map((doc) => doc.data())
+          .filter((message) => message.users.includes(selectedUser.uid));
+        setMessages(messagesData);
+      });
+
+      return unsubscribe;
+    }
+  }, [selectedUser, user]);
+
+
   function handleClick() {
-    setIsExpanded((prevExpanded) => !prevExpanded);
+    if (isExpanded) {
+      // If the chat box is expanded, reset selectedUser and collapse the chat box
+      setSelectedUser(null);
+      setIsExpanded(false);
+    } else {
+      // If the chat box is collapsed, expand the user list
+      setIsExpanded(true);
+    }
   }
 
-  // Function to handle user selection
-  function handleUserSelect(user) {
-    setSelectedUser(user);
+  function handleUserSelect(userId) {
+    // Find the selected user based on the userId
+    const selectedUser = users.find(user => user.userId === userId);
+    setSelectedUser(selectedUser);
     setIsExpanded(true);
   }
-
-  // Function to handle message submission
   async function handleSubmit(event) {
     event.preventDefault();
 
-    if (messageText.trim() !== "") {
+    if (messageText.trim() !== "" && selectedUser) {
       try {
-        // Display the sent message in the chat logs
-        const chatLogs = document.querySelector(".chat-logs");
-        const message = document.createElement("div");
-        message.className = "chat self";
-        message.innerHTML = messageText;
-        chatLogs.appendChild(message);
-        setMessageText("");
-
         // Save the message in the Firestore collection
         const messagesRef = collection(db, "messages");
         await addDoc(messagesRef, {
           text: messageText,
-          userId: user.uid,
+          users: [user.uid, selectedUser.uid],
           timestamp: serverTimestamp(),
         });
+
+        setMessageText("");
       } catch (error) {
         console.error("Error submitting message:", error.message);
       }
     }
   }
-
   // Render JSX based on user authentication state
   return (
     <>
@@ -102,41 +163,37 @@ function Message() {
 
           <div className={`chat-box ${isExpanded ? "expanded" : ""}`}>
             <div className="chat-box-header">
-              {selectedUser
-                ? `Messages with ${selectedUser.displayName}`
-                : "FlexiTalk"}
+              {selectedUser ? (
+                <>
+                  <i
+                    class="fa-solid fa-circle-arrow-left"
+                    onClick={() => setSelectedUser(null)}
+                  ></i>
+                  {selectedUser.displayName}
+                </>
+              ) : (
+                "FlexiTalk"
+              )}
               <span className="chat-box-toggle" onClick={handleClick}>
                 <i className="fa-solid fa-xmark"></i>
               </span>
             </div>
-            {users.length > 0 ? (
-              <div className="user-list">
-                <h2>Available Users</h2>
-                <ul>
-                  {users.map((user) => (
-                    <li
-                      key={user.id}
-                      onClick={() => handleUserSelect(user)}
-                    >
-                      {user.displayName}
-                    </li>
+            {isExpanded && selectedUser ? (
+              <div className="chat-box-body">
+                <div className="chat-logs">
+                  {messages.map((message, index) => (
+                    <div key={index} className={`chat ${message.userId === user.uid ? 'self' : 'other'}`}>
+                      {message.text}
+                    </div>
                   ))}
-                </ul>
-              </div>
-            ) : (
-              <div className="user-list">
-                <p>No available users</p>
-              </div>
-            )}
-            <div className="chat-box-body">
-              {isExpanded && selectedUser && (
+                </div>
                 <div className="chat-form">
-                  <div className="chat-logs"></div>
                   <input
                     type="text"
                     value={messageText}
                     onChange={(e) => setMessageText(e.target.value)}
                     placeholder="Send a message..."
+                    className="chat-input"
                   />
                   <button
                     type="submit"
@@ -146,8 +203,30 @@ function Message() {
                     <i className="material-icons">send</i>
                   </button>
                 </div>
-              )}
-            </div>
+              </div>
+            ) : loading ? (
+              <p>Loading...</p>
+            ) : (
+              <div className="user-list">
+                <h2>All Users</h2>
+                <ul>
+                  {users.map((user) => (
+                    <li
+                      key={user.id}
+                      onClick={() => handleUserSelect(user.userId)}
+                    >
+                      <div className="user-item">
+                        <img
+                          src={profileImages[user.userId]}
+                          className="profile-pic-chat"
+                        />
+                        <span>{user.displayName}</span>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
           </div>
         </div>
       ) : (
