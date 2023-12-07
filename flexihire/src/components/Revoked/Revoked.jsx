9e79da25f97679ee2315a8abcf52968e75726b91
Axel Xrest <52139213+AxelXrest@users.noreply.github.com>
2023-12-07 18:40:48 +0545

    router
    removed unused router

--- flexihire/src/components/Revoked/Revoked.jsx
+++ flexihire/src/components/Revoked/Revoked.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import ContactForm from "../ContactForm/ContactForm";
 import MailReceived from "../ContactForm/MailReceived";
 import Disabled from "./Disabled";
